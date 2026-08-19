39232deb1baf84a0e84fee9c6363ee203a0ab18b
j-petz <60827828+j-petz@users.noreply.github.com>
2026-08-19 09:10:39 +0200

    Setup base files and calculations

---
+++ script.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operatorName, firstNumber, secondNumber) {
+  switch (operatorName) {
+    case "add":
+      return add(firstNumber, secondNumber);
+      break;
+    case "subtract":
+      return subtract(firstNumber, secondNumber);
+      break;
+    case "multiply":
+      return multiply(firstNumber, secondNumber);
+      break;
+    case "divide":
+      return divide(firstNumber, secondNumber);
+      break;
+    default:
+      console.log("Oopsi!");
+  }
+}
