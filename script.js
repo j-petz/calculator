cdaf5f674cb62f9ce18ed6c03534badabc94ccc8
j-petz <60827828+j-petz@users.noreply.github.com>
2026-08-25 10:19:24 +0200

    Create function to calculate

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let currentNumber = 0;
+let currentOperator = "";
+let firstNumber = "";
+let secondNumber = "";
 
 function setOutput() {
   let output = document.querySelector(".output");
   output.textContent = currentNumber;
 }
 
-//setOutput();
 let numbersArray = [];
 
 function getNumbers() {
@@ -13,32 +15,78 @@ function getNumbers() {
   numberButtons.forEach((btn) =>
     btn.addEventListener("click", function (e) {
       let selectedNumber = e.target.textContent;
-      console.log(selectedNumber);
+      //console.log(selectedNumber);
       numbersArray.push(selectedNumber);
-      console.log(numbersArray);
+      //console.log(numbersArray);
       currentNumber = numbersArray.join("");
-      console.log(currentNumber);
+      //console.log(currentNumber);
       setOutput();
     }),
   );
 }
-
 getNumbers();
 
+function getOperator() {
+  const operatorButtons = document.querySelectorAll(
+    "#add, #subtract, #multiply, #divide",
+  );
+  operatorButtons.forEach((btn) =>
+    btn.addEventListener("click", function (e) {
+      currentOperator = e.target.id;
+      if (!firstNumber) {
+        firstNumber = currentNumber;
+        numbersArray = [];
+        setOutput();
+        console.log(`Current Operator: ${currentOperator}`);
+        console.log(`First Number: ${firstNumber}`);
+        console.log(`Current Number: ${currentNumber}`);
+      } else {
+        secondNumber = currentNumber;
+        numbersArray = [];
+        console.log(`Second Number: ${secondNumber}`);
+      }
+    }),
+  );
+}
+getOperator();
+
+function calculate() {
+  let enter = document.querySelector("#enter");
+  enter.addEventListener("click", function (e) {
+    secondNumber = currentNumber;
+    console.log(`Current Operator: ${currentOperator}`);
+    console.log(`First Number: ${firstNumber}`);
+    console.log(`Second Number: ${secondNumber}`);
+    console.log(`Current Number: ${currentNumber}`);
+    operate(currentOperator, firstNumber, secondNumber);
+    firstNumber = currentNumber;
+  });
+}
+calculate();
+
 function add(a, b) {
-  return a + b;
+  currentNumber = parseInt(a) + parseInt(b);
+  console.log(`Current Number: ${currentNumber}`);
+  setOutput();
+  //return a + b;
 }
 
 function subtract(a, b) {
-  return a - b;
+  currentNumber = parseInt(a) - parseInt(b);
+  setOutput();
+  //return a - b;
 }
 
 function multiply(a, b) {
-  return a * b;
+  currentNumber = parseInt(a) * parseInt(b);
+  setOutput();
+  //return a * b;
 }
 
 function divide(a, b) {
-  return a / b;
+  currentNumber = parseInt(a) / parseInt(b);
+  setOutput();
+  //return a / b;
 }
 
 function operate(operatorName, firstNumber, secondNumber) {
@@ -55,7 +103,7 @@ function operate(operatorName, firstNumber, secondNumber) {
     case "divide":
       return divide(firstNumber, secondNumber);
       break;
-    default:
-      console.log("Oops!");
+    /* default:
+      console.log("Oops!"); */
   }
 }
